af02075517cd00d7383d7915732ca6cac25731ac
Fardan-Nurhidayat <fardannurhidayat07.stu@pnc.ac.id>
2025-07-28 12:58:04 +0700

    feat : add landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,39 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: "#D94336",
+                primaryLight: "#C33C30",
+                secondary: "#FDF8F0",
+                third: "#3A5A40",
+                text: "#333333",
+                primaryBg: "#EDEFEF",
             },
         },
     },
     variants: {
         extend: {
-            backgroundColor: ['active'],
-        }
+            backgroundColor: ["active"],
+        },
     },
     content: [
-        './app/**/*.php',
-        './resources/**/*.html',
-        './resources/**/*.js',
-        './resources/**/*.jsx',
-        './resources/**/*.ts',
-        './resources/**/*.tsx',
-        './resources/**/*.php',
-        './resources/**/*.vue',
-        './resources/**/*.twig',
+        "./app/**/*.php",
+        "./resources/**/*.html",
+        "./resources/**/*.js",
+        "./resources/**/*.jsx",
+        "./resources/**/*.ts",
+        "./resources/**/*.tsx",
+        "./resources/**/*.php",
+        "./resources/**/*.vue",
+        "./resources/**/*.twig",
     ],
     plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
     ],
-}
+};
